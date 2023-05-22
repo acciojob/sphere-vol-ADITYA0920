92629e8373f7e0df43e97e43a3dae0f3f5c2dc4c
Aditya Mane <112119500+ADITYA0920@users.noreply.github.com>
2023-05-22 19:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.getElmentById("radius").value ;
+
+	let res = 4/3 * 3.14 * (radius * radius );
+
+	let volume = document.getElementById("volume");
+	volume.innerText = res ;
+
+	
+	
   
 } 
 
